a4c0dfef2ddf2c98ee7d1aaf86e4cfcb32e1f435
SergeyErshov <s.ershov.shoko@gmail.com>
2022-12-08 21:44:10 +0300

    use file_get_contents instead curl; use variable for block_id

--- sber/payment.php
+++ sber/payment.php
@@ -6,176 +6,110 @@ Bitrix\Main\Loader::includeModule('sale');
 Bitrix\Main\Loader::includeModule('catalog');
 Bitrix\Main\Loader::IncludeModule('iblock');
 $el = new CIBlockElement;
-//$companies = Array();
-
-//$companies[1] = Array();
-//$companies[1]["USERNAME"] = "avantern-api";
-//$companies[1]["PASSWORD"] = "avntrN15*";
-
-//$companies[2] = Array();
-//$companies[2]["USERNAME"] = "avantern3-api";
-//$companies[2]["PASSWORD"] = "3avntrN15*";
-
-//if($_REQUEST["send"] == "Y")
-//{
-
-
-  
-    $arResult = Array();
-    $company = trim($_REQUEST["company"]);
-    $service = trim($_REQUEST["service"]);
-    $name = trim($_REQUEST["name"]);
-    $num = trim($_REQUEST["dogovor"]);
-	$email = trim($_REQUEST["email"]);
-	$phone = trim($_REQUEST["phone"]);
-    $rub = intval(trim($_REQUEST["rub"]));
-    $kop = intval(trim($_REQUEST["kop"]));
-    
-    $sum = $rub*100+$kop;
-	$description = 'Оплата услуг по договору '.$num.'. Услуга: '.$service.'. Плательщик: '.$name.'. E-mail: '.$email;
-  
+$cib_id="6"; //задаем ИД информационного блока
+$ps_key=file_get_contents('/home/bitrix/.metadata/payler_key', false, null, 0); //извлекаем в строку все содержимое файла с ключем платежной системы
 
 /*
-//$products = array(array('PRODUCT_ID' => 1, 'NAME' => $description, 'PRICE' => $rub."."$kop, 'CURRENCY' => 'RUB', 'QUANTITY' => 1));
-        
-$basket = Bitrix\Sale\Basket::create(SITE_ID);
-
-foreach ($products as $product)
-    {
-        $item = $basket->createItem("catalog", $product["PRODUCT_ID"]);
-        unset($product["PRODUCT_ID"]);
-        $item->setFields($product);
-    }
-    
-$order = Bitrix\Sale\Order::create(SITE_ID, 1);
-$order->setPersonTypeId(1);
-$order->setBasket($basket);
-
-$shipmentCollection = $order->getShipmentCollection();
-$shipment = $shipmentCollection->createItem(
-        Bitrix\Sale\Delivery\Services\Manager::getObjectById(1)
-    );
-
-$shipmentItemCollection = $shipment->getShipmentItemCollection();
-
-/** @var Sale\BasketItem $basketItem */
-/*
-foreach ($basket as $basketItem)
-    {
-   		$item = $shipmentItemCollection->createItem($basketItem);
-        $item->setQuantity($basketItem->getQuantity());
-    }
-
-$paymentCollection = $order->getPaymentCollection();
-$payment = $paymentCollection->createItem(
-        Bitrix\Sale\PaySystem\Manager::getObjectById(5)
-    );
-$payment->setField("SUM", $order->getPrice());
-$payment->setField("CURRENCY", $order->getCurrency());
-    
-$result = $order->save();
-    if (!$result->isSuccess())
-        {
-            //$result->getErrors();
-        }
-$orderId = $order->getId();
-//echo $orderId;
+$arResult = Array();
+$company = trim($_REQUEST["company"]);
+$service = trim($_REQUEST["service"]);
+$name = trim($_REQUEST["name"]);
+$num = trim($_REQUEST["dogovor"]);
+$email = trim($_REQUEST["email"]);
+$phone = trim($_REQUEST["phone"]);
+$rub = intval(trim($_REQUEST["rub"]));
+$kop = intval(trim($_REQUEST["kop"]));
 */
- 
-   /* $url = 'https://3dsec.paymentgate.ru/ipay/rest/register.do';
 
-    $params = array(
-        'userName' => $companies[$company]["USERNAME"], 
-        'password' => $companies[$company]["PASSWORD"], 
-        'orderNumber' => (int) date('YmdHis'),
-        'description' => $description,
-        'amount' => $sum,
-        'returnUrl' => 'http://pay.avantern.ru/success/'
-    );
-    $result = file_get_contents($url, false, stream_context_create(array(
-        'http' => array(
-            'method'  => 'POST',
-            'header'  => 'Content-type: application/x-www-form-urlencoded',
-            'content' => http_build_query($params)
+$service = "За мониторинг";
+$name = "Иван Иванов";
+$num = "T-1234";
+$email = "i.ivanov@noemail";
+$rub = "10";
+$kop = "00";
+$orderId = "test-0812222123";
+
+$sum = $rub*100+$kop;
+$description = 'Оплата услуг по договору '.$num.'. Услуга: '.$service.'. Плательщик: '.$name.'. E-mail: '.$email;
+  
+//$orderId=time();
+
+$qUrl = 'https://secure.payler.com/gapi/StartSession?';
+$qData = http_build_query(array(
+    'key' => $ps_key, 
+	'type' =>1,
+	'order_id'=>$orderId,
+	'amount'=>$sum,
+	'product'=>$description,
+	'userdata'=>$description,
+	'email'=>$email,
+	'total'=>1,
+	'lang'=>'ru'));
+
+$result = file_get_contents($qUrl.http_build_query($qData), false, stream_context_create(array(
+    'http' => array(
+        'method'  => 'POST',
+        'header'  => 'Content-type: application/x-www-form-urlencoded',
+        'content' => http_build_query($qData)
         )
     )));
-    
-    $result = json_decode($result);
-	
-    
-    if(strlen($result->orderId) > 0 && strlen($result->formUrl) > 0)
-    {
-        $arResult["OK"] = "Y";
-        $arResult["formUrl"] = $result->formUrl;
-    }
-    else
-        $arResult["OK"] = "N";
-        
-    $arResult = json_encode($arResult);
-    
-    echo $arResult;*/
-//$sum=100;
 
-$orderId=time();
+    /*
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_SSL_VERIFYPEER => 0,
+    CURLOPT_POST => 1,
+    CURLOPT_HEADER => 0,
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $queryUrl,
+    CURLOPT_POSTFIELDS => $queryData,
+    ));
+$result = curl_exec($curl);
+curl_close($curl);
+*/
+$result = json_decode($result);
 
-
-$queryUrl = 'https://secure.payler.com/gapi/StartSession?';
- $queryData = http_build_query(array(
-        'key' => '81841953-4e79-4123-9272-ecdac2b9c5fa', 
-//		'password'=>'ZhGNYXdCxA',
-		'type' =>1,
-		'order_id'=>$orderId,
-		'amount'=>$sum,
-		'product'=>$description,
-		'userdata'=>$description,
-		'email'=>$email,
-		'total'=>1,
-		'lang'=>'ru'));
-
- $curl = curl_init();
- curl_setopt_array($curl, array(
- CURLOPT_SSL_VERIFYPEER => 0,
- CURLOPT_POST => 1,
- CURLOPT_HEADER => 0,
- CURLOPT_RETURNTRANSFER => 1,
- CURLOPT_URL => $queryUrl,
- CURLOPT_POSTFIELDS => $queryData,
- ));
-
- $result = curl_exec($curl);
- $result = json_decode($result);
 // echo "<pre>"; print_r($result); echo "</pre>";
- curl_close($curl);
-
-
-
-
- //   $res = json_decode($res);
+// $res = json_decode($res);
 //	print_r($result->session_id);
-	$session_id=$result->session_id;
-    $url = 'https://secure.payler.com/gapi/Pay?session_id='.$session_id;
- header('Refresh: 1; url="'.$url.'"');
-  //  echo "Через 5 секунд вы будите переведены на платежный шлюз банка))";
+
+$session_id=$result->session_id;
+
+$url = 'https://secure.payler.com/gapi/Pay?session_id='.$session_id;
+
+header('Refresh: 1; url="'.$url.'"');
+
+//  echo "Через 5 секунд вы будите переведены на платежный шлюз банка))";
 //	LocalRedirect($url);
 
-$queryUrl = 'https://secure.payler.com/gapi/GetStatus';
- $queryData = http_build_query(array(
-        'key' => '81841953-4e79-4123-9272-ecdac2b9c5fa', 
-		'order_id'=>$orderId
-        ));
+$qUrl = 'https://secure.payler.com/gapi/GetStatus';
+$qData = http_build_query(array(
+    'key' => $ps_key, 
+	'order_id'=>$orderId
+    ));
 
- $curl = curl_init();
- curl_setopt_array($curl, array(
- CURLOPT_SSL_VERIFYPEER => 0,
- CURLOPT_POST => 1,
- CURLOPT_HEADER => 0,
- CURLOPT_RETURNTRANSFER => 1,
- CURLOPT_URL => $queryUrl,
- CURLOPT_POSTFIELDS => $queryData,
- ));
+$result = file_get_contents($qUrl.http_build_query($qData), false, stream_context_create(array(
+    'http' => array(
+        'method'  => 'POST',
+        'header'  => 'Content-type: application/x-www-form-urlencoded',
+        'content' => http_build_query($qData)
+        )
+    )));
+
+/*
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_SSL_VERIFYPEER => 0,
+    CURLOPT_POST => 1,
+    CURLOPT_HEADER => 0,
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $queryUrl,
+    CURLOPT_POSTFIELDS => $queryData,
+));
 
 $result1 = curl_exec($curl);
 curl_close($curl);
+*/
 
 $result1 = json_decode($result);
 
@@ -183,7 +117,7 @@ $status = $result1->status;
 
 echo $result1->status;
 
-
+/*
 $PROP = array();
 //ФИО
 $PROP[1]=$name;
@@ -202,19 +136,18 @@ $PROP[7]=$orderId;
 //Статус операции
 $PROP[8]=$status;
 
-		 $arLoadProductArray = Array(
+$arLoadProductArray = Array(
   "MODIFIED_BY"    => $USER->GetID(), // элемент изменен текущим пользователем
   "IBLOCK_SECTION_ID" => false,          // элемент лежит в корне раздела
-  "IBLOCK_ID"      => 6,
+  "IBLOCK_ID"      => $cib_id,
   "PROPERTY_VALUES"=> $PROP,
   "NAME"           => $description,
   "ACTIVE"         => "N"            //пока не активен
   );
 
 
-$PRODUCT_ID = $el->Add($arLoadProductArray);
+//$PRODUCT_ID = $el->Add($arLoadProductArray);
+*/
 
 ?>
-
-
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_after.php");?>
